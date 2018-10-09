2a7f2a729d3a6cb521ab237247bdf43191d21da1
Aaron Kammula <aaron.kammula@gmail.com>
2018-10-09 02:34:37 -0400

    Started working on the homepage routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	root "home#index" #Calls index method of home controller, then displays index view for home
+    root "home#index" #Calls index method of home controller, then displays index view for home
     get 'home/about' #Creates a new route called "home_about"
     get 'home/login'
     get 'home/signup'
