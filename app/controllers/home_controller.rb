577d77a385d2b5350c13eaa5945958dbe11f2306
Aaron Kammula <aaron.kammula@gmail.com>
2018-10-09 02:32:20 -0400

    Forgot to do git add :P. Home page should show now

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+  def index
+  end
+    
+  def about #Helps me link_to the about page
+  end 
+
+  def login
+  end 
+
+  def signup
+  end
+
+  def show
+  end	
+
+end
