5ec3a693d377c4a90afc66debae963049d531a48
yoyo-wu <yoyo.wu@mail.utoronto.ca>
2018-11-28 14:29:53 -0500

    Added js folder for javascript components from template. Changed navigation bar for log in and log out functions.

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,5 +1,16 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
+  def setup
+    @item = Item.new(user_id: 3, deposit: 40, name: "test", description: "test description")
+  end
 
+  test "should be valid" do
+    assert @item.valid?
+  end
+
+  test "user id should be present" do
+    @item.user_id = nil
+    assert_not @item.valid?
+  end
 end
