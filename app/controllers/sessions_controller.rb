aafcb858dc287668062bbfe13ec4a3eade35d2a6
JiachenYu15 <milesyjc@gmail.com>
2018-11-28 15:51:47 -0800

    Redirect to personal home after login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 
         if user && user.authenticate(params[:session][:password])
             log_in user
-            redirect_to user
+            redirect_to personal_home_index_path
         else
             flash.now[:danger] = 'Invalid email/password combination'
             render 'new'
