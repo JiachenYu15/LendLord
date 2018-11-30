60341df122ecf2b0cc991ad293ed9f9e9de63070
JiachenYu15 <milesyjc@gmail.com>
2018-11-30 13:12:09 -0800

    Add some css for personal home page.

--- db/schema.rb
+++ db/schema.rb
@@ -46,15 +46,6 @@ ActiveRecord::Schema.define(version: 2018_11_27_234539) do
     t.integer "score"
   end
 
-  create_table "transaction_ratings", force: :cascade do |t|
-    t.string "username"
-    t.integer "transaction"
-    t.boolean "role"
-    t.datetime "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "transactions", force: :cascade do |t|
     t.integer "item_id"
     t.integer "borrow_from_user_id"
