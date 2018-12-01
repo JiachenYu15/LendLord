a295c72c6e01d2225e7147751de37127922e8d69
lendlord <victor.vallejoricaurte@mail.utoronto.ca>
2018-12-01 00:19:20 -0500

    Enabling flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,6 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include SessionsHelper
 
+    add_flash_types :danger, :info, :warning, :success
+
 end
