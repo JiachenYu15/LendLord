6b18d6219d13d739b9a109978859f474ebe40b76
CalixZhangECE <calix.zhang@mail.utoronto.ca>
2018-12-03 16:28:11 -0800

    Made login not required for home pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-    before_action :authenticate_user!, :except => [:show, :index]
+    before_action :authenticate_user!, :except => [:show, :index, :about, :contact, :faq, :privacy_policy, :terms_of_service]
     before_action :require_login, :except => [:show, :index]
     #include SessionHelper
 
