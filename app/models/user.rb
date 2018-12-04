d6aa4bb15d1c9f1a7cc612dc4a9e475b2e03c878
CalixZhangECE <calix.zhang@mail.utoronto.ca>
2018-12-04 05:35:45 -0800

    Added regex and Terms of Service check

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, 
          :omniauthable, omniauth_providers: %i[facebook]
 
+  validates :terms_of_service, acceptance: true
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
